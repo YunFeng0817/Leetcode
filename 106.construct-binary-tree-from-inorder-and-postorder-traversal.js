acc09c089f1de14cd4ce6288f4ece3055563ac50
Mark-fenng <f18846188605@gmail.com>
2020-09-25 14:58:05 +0800

    Finished the question #106

---
+++ 106.construct-binary-tree-from-inorder-and-postorder-traversal.js
@@ -0,0 +1,71 @@
+/*
+ * @lc app=leetcode id=106 lang=javascript
+ *
+ * [106] Construct Binary Tree from Inorder and Postorder Traversal
+ *
+ * https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/description/
+ *
+ * algorithms
+ * Medium (46.98%)
+ * Likes:    2073
+ * Dislikes: 38
+ * Total Accepted:    254.5K
+ * Total Submissions: 531.7K
+ * Testcase Example:  '[9,3,15,20,7]\n[9,15,7,20,3]'
+ *
+ * Given inorder and postorder traversal of a tree, construct the binary tree.
+ *
+ * Note:
+ * You may assume that duplicates do not exist in the tree.
+ *
+ * For example, given
+ *
+ *
+ * inorder = [9,3,15,20,7]
+ * postorder = [9,15,7,20,3]
+ *
+ * Return the following binary tree:
+ *
+ *
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} inorder
+ * @param {number[]} postorder
+ * @return {TreeNode}
+ */
+var buildTree = function (inorder, postorder) {
+    if (inorder.length < 1) return null;
+    let root = new TreeNode();
+    let rootVal = postorder[postorder.length - 1];
+    root.val = rootVal;
+    let rootIndex = inorder.indexOf(rootVal);
+    let leftInorder = inorder.slice(0, rootIndex);
+    if (leftInorder.length > 0) {
+        root.left = buildTree(leftInorder, postorder.slice(0, leftInorder.length));
+    }
+    let rightInorder = inorder.slice(rootIndex + 1);
+    if (rightInorder.length > 0) {
+        root.right = buildTree(rightInorder, postorder.slice(leftInorder.length, postorder.length - 1));
+    }
+    return root;
+};
+// @lc code=end
+// []\n[]
+// [1]\n[1]
