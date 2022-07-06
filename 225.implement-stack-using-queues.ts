e1cc0e23e046b7cbef1907072597d8b932edd320
Yun Feng <yun.feng@smartx.com>
2022-07-06 14:43:26 +1000

    #225 Implement Stack using Queues

---
+++ 225.implement-stack-using-queues.ts
@@ -0,0 +1,115 @@
+/*
+ * @lc app=leetcode id=225 lang=typescript
+ *
+ * [225] Implement Stack using Queues
+ *
+ * https://leetcode.com/problems/implement-stack-using-queues/description/
+ *
+ * algorithms
+ * Easy (56.02%)
+ * Likes:    3053
+ * Dislikes: 883
+ * Total Accepted:    356.5K
+ * Total Submissions: 636K
+ * Testcase Example:  '["MyStack","push","push","top","pop","empty"]\n[[],[1],[2],[],[],[]]'
+ *
+ * Implement a last-in-first-out (LIFO) stack using only two queues. The
+ * implemented stack should support all the functions of a normal stack (push,
+ * top, pop, and empty).
+ *
+ * Implement the MyStack class:
+ *
+ *
+ * void push(int x) Pushes element x to the top of the stack.
+ * int pop() Removes the element on the top of the stack and returns it.
+ * int top() Returns the element on the top of the stack.
+ * boolean empty() Returns true if the stack is empty, false otherwise.
+ *
+ *
+ * Notes:
+ *
+ *
+ * You must use only standard operations of a queue, which means that only push
+ * to back, peek/pop from front, size and is empty operations are valid.
+ * Depending on your language, the queue may not be supported natively. You may
+ * simulate a queue using a list or deque (double-ended queue) as long as you
+ * use only a queue's standard operations.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input
+ * ["MyStack", "push", "push", "top", "pop", "empty"]
+ * [[], [1], [2], [], [], []]
+ * Output
+ * [null, null, null, 2, 2, false]
+ *
+ * Explanation
+ * MyStack myStack = new MyStack();
+ * myStack.push(1);
+ * myStack.push(2);
+ * myStack.top(); // return 2
+ * myStack.pop(); // return 2
+ * myStack.empty(); // return False
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 1 <= x <= 9
+ * At most 100 calls will be made to push, pop, top, and empty.
+ * All the calls to pop and top are valid.
+ *
+ *
+ *
+ * Follow-up: Can you implement the stack using only one queue?
+ *
+ */
+
+// @lc code=start
+class MyStack {
+  queue1: number[] = [];
+  queue2: number[] = [];
+  usingFirst: boolean = true;
+  constructor() {}
+
+  push(x: number): void {
+    if (this.usingFirst) this.queue1.push(x);
+    else this.queue2.push(x);
+  }
+
+  pop(): number {
+    const top = this.top();
+    const usingQueue = this.usingFirst ? this.queue1 : this.queue2;
+    usingQueue.shift();
+    this.usingFirst = !this.usingFirst;
+    return top;
+  }
+
+  top(): number {
+    const usingQueue = this.usingFirst ? this.queue1 : this.queue2;
+    const backupQueue = this.usingFirst ? this.queue2 : this.queue1;
+    while (usingQueue.length > 1) {
+      const peek = usingQueue.shift();
+      backupQueue.push(peek!);
+    }
+    return usingQueue[0];
+  }
+
+  empty(): boolean {
+    return this.queue1.length == 0 && this.queue2.length === 0;
+  }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * var obj = new MyStack()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.empty()
+ */
+// @lc code=end
