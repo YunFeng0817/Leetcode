32ef86857f78423ce77a417e084fb88464ca39c5
Yun Feng <yun.feng0817@gmail.com>
2023-11-29 12:47:13 +0800

    #274 H-Index

---
+++ 274.h-index.ts
@@ -0,0 +1,64 @@
+/*
+ * @lc app=leetcode id=274 lang=typescript
+ *
+ * [274] H-Index
+ *
+ * https://leetcode.com/problems/h-index/description/
+ *
+ * algorithms
+ * Medium (38.44%)
+ * Likes:    874
+ * Dislikes: 323
+ * Total Accepted:    360.1K
+ * Total Submissions: 933.8K
+ * Testcase Example:  '[3,0,6,1,5]'
+ *
+ * Given an array of integers citations where citations[i] is the number of
+ * citations a researcher received for their i^th paper, return the
+ * researcher's h-index.
+ *
+ * According to the definition of h-index on Wikipedia: The h-index is defined
+ * as the maximum value of h such that the given researcher has published at
+ * least h papers that have each been cited at least h times.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: citations = [3,0,6,1,5]
+ * Output: 3
+ * Explanation: [3,0,6,1,5] means the researcher has 5 papers in total and each
+ * of them had received 3, 0, 6, 1, 5 citations respectively.
+ * Since the researcher has 3 papers with at least 3 citations each and the
+ * remaining two with no more than 3 citations each, their h-index is 3.
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: citations = [1,3,1]
+ * Output: 1
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * n == citations.length
+ * 1 <= n <= 5000
+ * 0 <= citations[i] <= 1000
+ *
+ *
+ */
+
+// @lc code=start
+function hIndex(citations: number[]): number {
+  citations.sort((a, b) => b - a);
+  let h = 0;
+  for (let i = 0; i < citations.length; i++) {
+    if (citations[i] >= i + 1) h = i + 1;
+    else break;
+  }
+  return h;
+}
+// @lc code=end
